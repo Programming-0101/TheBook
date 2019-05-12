b9477a70fc3a437011a6c7f7e71dbf2bcd5a88f6
dagilleland <dagilleland@users.noreply.github.com>
2019-05-12 06:02:11 -0600

    More work on the catalog of types

--- .vuepress/components/topic-types.vue
+++ .vuepress/components/topic-types.vue
@@ -5,6 +5,13 @@
     </header>
     <main>
       <i>Coming Soon:</i> Alpha-testing vue component to process json list of data types used in the code examples and practices. There are {{types.length}} data types registered.
+      <ul>
+        <li v-for="item in types">
+          {{ item.Type }}
+          -
+          <b>Examples</b>: <span v-for="ex in item.Examples"><a :href="ex.Topic + '/' + ex.Doc">{{ex.Topic}}</a></span>
+        </li>
+      </ul>
     </main>
     <footer>
       
