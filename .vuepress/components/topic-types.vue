1722aecec38cd5120e893bc6c234e5acf257eec0
dagilleland <dagilleland@users.noreply.github.com>
2019-10-11 17:28:42 -0600

    Vue component - show Practice links

--- .vuepress/components/topic-types.vue
+++ .vuepress/components/topic-types.vue
@@ -10,6 +10,8 @@
           {{ item.Type }}
           -
           <b>Examples</b>: <span v-for="ex in item.Examples"><a :href="ex.Topic + '/' + ex.Doc">{{ex.Topic}}</a></span>
+          -
+          <b>Practice</b>: <span v-for="ex in item.Practice"><a :href="ex.Topic + '/' + ex.Doc">{{ex.Topic}}</a></span>
         </li>
       </ul>
     </main>
