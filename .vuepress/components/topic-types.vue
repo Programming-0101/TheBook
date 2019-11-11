f35f91a88cf7a1b20df17dc2131c777524612e4d
dagilleland <dagilleland@users.noreply.github.com>
2019-11-11 15:24:13 -0700

    Fix undefined in path for topic pages

--- .vuepress/components/topic-types.vue
+++ .vuepress/components/topic-types.vue
@@ -9,9 +9,9 @@
         <li v-for="item in types">
           {{ item.Type }}
           -
-          <b>Examples</b>: <span v-for="ex in item.Examples"><a :href="ex.Topic + '/Examples/' + ex.Doc">{{ex.Topic}}</a></span>
+          <b>Examples</b>: <span v-for="ex in item.Examples"><a :href="ex.Topic + '/Examples/' + (ex.Doc || '')">{{ex.Topic}}</a></span>
           -
-          <b>Practice</b>: <span v-for="ex in item.Practice"><a :href="ex.Topic + '/Practice/' + ex.Doc">{{ex.Topic}}</a></span>
+          <b>Practice</b>: <span v-for="ex in item.Practice"><a :href="ex.Topic + '/Practice/' + (ex.Doc || '')">{{ex.Topic}}</a></span>
         </li>
       </ul>
     </main>
